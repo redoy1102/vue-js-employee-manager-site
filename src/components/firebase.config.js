788b14b00d259b5800dcd08fc018e720496063c6
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2021-01-03 16:04:30 +0600

    Firestore, firebaseconfig,firebaseinit added

---
+++ src/components/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDhPcwlJF4Pck08G9_8wmKn6AghhGhpt0Q",
+  authDomain: "employee-manager-7c6b5.firebaseapp.com",
+  projectId: "employee-manager-7c6b5",
+  storageBucket: "employee-manager-7c6b5.appspot.com",
+  messagingSenderId: "821558456413",
+  appId: "1:821558456413:web:1247f5a0272df5f3021db4"
+}
